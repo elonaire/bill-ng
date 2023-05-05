09b28982337f3a0f0808bf6411a2ee562d93cae0
Elon Aseneka <elonsantos63@gmail.com>
2023-05-05 05:54:17 +0300

    demo folder ignore

--- src/app/demo/components/auth/newpassword/newpassword-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NewPasswordComponent } from './newpassword.component';
-
-@NgModule({
-    imports: [RouterModule.forChild([{ path: '', component: NewPasswordComponent }])],
-    exports: [RouterModule]
-})
-export class NewPasswordRoutingModule {}
--- src/app/demo/components/pages/help/help-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HelpComponent } from './help.component';
-
-@NgModule({
-    imports: [RouterModule.forChild([{ path: '', component: HelpComponent }])],
-    exports: [RouterModule]
-})
-export class HelpRoutingModule {}
